afbcb1a2d62f9b8fc693bc84873df984d2e57ad6
Aydin ABIAR <aa9380@b-3-40.c.hpc-slurm-9c75.internal>
2022-11-20 15:07:10 -0500

    Add modifier main file + results experiments

--- gpu-mem-usage/mnist/test-w-ncu.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ncu --profile-from-start off --metrics smsp__sass_thread_inst_executed_op_fadd_pred_on,smsp__sass_thread_inst_executed_op_fmul_pred_on,smsp__sass_thread_inst_executed_op_ffma_pred_on --target-processes all python3 main_ncu.py --dry-run --batch-size 128 --epochs 2 2>&1 | tee ncu.out
-
-
-
